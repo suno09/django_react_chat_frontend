04a2ae0874c3aa13e93d51ca072edbb4ce9e065d
suno09 <shemsou9@gmail.com>
2021-12-25 16:52:32 +0100

    initial coomit

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import './App.css';
+import InitChat from './components/InitChat'
+import Chat from './components/Chat'
+import WebSocketInstance from './services/WebSocket'
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+      username: '',
+      loggedIn: false
+    };
+  }
+
+  handleLoginSubmit = (username) => {
+    this.setState({ loggedIn: true, username: username });
+    WebSocketInstance.connect();
+  }
+
+  render() {
+    const { 
+      loggedIn,
+      username
+    } = this.state;
+
+    return (
+      <div className="App">
+        { 
+          loggedIn ?
+          <Chat
+            username1="admin"
+            username2="admin2"
+          />
+          :
+          <InitChat
+            onSubmit={this.handleLoginSubmit}
+            usernameChangeHandler={this.usernameChangeHandler}
+          />
+        }
+      </div>
+    );
+  }
+}
---
+++ src/services/WebSocket.js
@@ -0,0 +1,104 @@
+class WebSocketService {
+  static instance = null;
+  static username1 = null;
+  static username2 = null;
+  callbacks = {};
+
+  static getInstance() {
+    if (!WebSocketService.instance) {
+      WebSocketService.instance = new WebSocketService();
+    }
+    return WebSocketService.instance;
+  }
+
+  constructor() {
+    this.socketRef = null;
+  }
+
+  connect() {
+    const path = 'ws://localhost:8000/ws/chat';
+    this.socketRef = new WebSocket(path);
+    this.socketRef.onopen = () => {
+      console.log('WebSocket open');
+    };
+    this.socketRef.onmessage = e => {
+      this.socketNewMessage(e.data);
+    };
+
+    this.socketRef.onerror = e => {
+      console.log(e.message);
+    };
+    this.socketRef.onclose = () => {
+      console.log("WebSocket closed let's reopen");
+      this.connect();
+    };
+  }
+
+  socketNewMessage(data) {
+    const parsedData = JSON.parse(data);
+    const command = parsedData.command;
+    if (Object.keys(this.callbacks).length === 0) {
+      return;
+    }
+    if (command === 'messages') {
+      this.callbacks[command](parsedData.messages);
+    }
+    if (command === 'new_message') {
+      this.callbacks[command](parsedData.message);
+    }
+  }
+
+  initChatUser(username1, username2) {
+    this.sendMessage({ command: 'init_chat', username1: username1, username2: username2 });
+  }
+
+  fetchMessages(username1, username2) {
+    this.sendMessage({ command: 'fetch_messages', username1: username1, username2: username2 });
+  }
+
+  newChatMessage(message) {
+    this.sendMessage({ command: 'new_message', from: message.from, text: message.text }); 
+  }
+
+  addCallbacks(messagesCallback, newMessageCallback) {
+    this.callbacks['messages'] = messagesCallback;
+    this.callbacks['new_message'] = newMessageCallback;
+  }
+  
+  sendMessage(data) {
+    try {
+      this.socketRef.send(JSON.stringify({ ...data }));
+    }
+    catch(err) {
+      console.log(err.message);
+    }  
+  }
+
+  state() {
+    return this.socketRef.readyState;
+  }
+
+   waitForSocketConnection(callback){
+    const socket = this.socketRef;
+    const recursion = this.waitForSocketConnection;
+    setTimeout(
+      function () {
+        if (socket.readyState === 1) {
+          console.log("Connection is made")
+          if(callback != null){
+            callback();
+          }
+          return;
+
+        } else {
+          console.log("wait for connection...")
+          recursion(callback);
+        }
+      }, 1); // wait 5 milisecond for the connection...
+  }
+
+}
+
+const WebSocketInstance = WebSocketService.getInstance();
+
+export default WebSocketInstance;
